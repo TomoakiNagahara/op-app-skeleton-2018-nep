48791dc0ce9cd654bcc4ec08c05e62d90e732e55
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-05-24 17:42:21 +0900

    Fix: asset/test/unit

--- asset/test/unit/database/orm.php
+++ asset/test/unit/database/orm.php
@@ -28,49 +28,67 @@ if( $ai = ifset($_GET['ai']) ){
 	$record = $orm->Create("t_orm");
 }
 
-//	Save to database by Record object.
-$result = $record->Validate() ? $orm->Save($record): null;
+//	Generate form object.
+$form = $record->Form();
 
-//	...
+//	Save to database by Record object.
+$result = $form->Validate() ? $orm->Save($record): null;
+
+//	Debug
 D('ai', $ai);
 D('result', $result);
 D('Found', $record->isFound());
-D('Validate', $record->isValid(), $record->Validate());
+D('Validate', $record->isValid(), $record->Validate(), $form->Validate());
 
-//	Generate form object.
-$form = $record->Form();
 ?>
+<section id="testcase">
+	<p class="<?= $record->isFound() ? 'blue':'red' ?>">Found record</p>
+	<p class="<?= $record->isValid() ? 'blue':'red' ?>">Validate</p>
+	<p class="<?= $result !== false  ? 'blue':'red' ?>">Result</p>
 
-<p class="<?= $record->isFound() ? 'blue':'red' ?>">Found record</p>
-<p class="<?= $record->isValid() ? 'blue':'red' ?>">Validate</p>
-<p class="<?= $result            ? 'blue':'red' ?>">Result</p>
+	<?php $form->Start() ?>
+	<table>
+		<?php foreach( ['number','integer','positive','text'] as $name ): ?>
+		<tr>
+			<th><?= $form->Label($name) ?></th>
+			<td><?= $form->Input($name) ?></td>
+			<td><?= $form->Error($name) ?></td>
+		</tr>
+		<?php endforeach; ?>
+		<tr>
+			<td colspan=2 style="text-align: center;">
+				<button>
+					<?= !$ai ? 'Create':'Update' ?>
+				</button>
+			</td>
+		</tr>
+	</table>
+	<?php $form->Finish() ?>
+	<hr/>
+	<?php
+	//	...
+	foreach( $db->Quick('t_orm','order=timestamp desc, limit=10') as $temp ){
+		printf('<a href="?ai=%s">Edit of ai is %s</a>', $temp['ai'], $temp['ai']);
+		D($temp);
+	}
+	?>
+</section>
+<style>
 
-<?php $form->Start() ?>
-<table>
-	<?php foreach( ['number','text'] as $name ): ?>
-	<tr>
-		<th><?= $form->Label($name) ?></th>
-		<td><?= $form->Input($name) ?></td>
-		<td><?= $form->Error($name) ?></td>
-	</tr>
-	<?php endforeach; ?>
-	<tr>
-		<td colspan=3 style="text-align: center;">
-			<button>
-				<?= !$ai ? 'Create':'Update' ?>
-			</button>
-		</td>
-	</tr>
-</table>
-<?php $form->Finish() ?>
-<hr/>
-<?php
-//	...
-foreach( $db->Quick('t_orm','order=timestamp desc, limit=10') as $record ){
-	printf('<a href="?ai=%s">Edit of ai is %s</a>', $record['ai'], $record['ai']);
-	D($record);
+#testcase th,
+#testcase td {
+	padding: 0 0.5em;
 }
 
+#testcase td:nth-child(3) > span:nth-child(2) {
+	display: none;
+}
+
+</style>
+<?
 //	...
-$orm->Debug();
-//$record->Debug();
+if( $_GET['debug'] ?? null ){
+	$orm->Debug();
+	$form->Debug();
+	$record->Debug();
+}
