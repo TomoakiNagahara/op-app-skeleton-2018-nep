33554876c7a0bf0fefd8348bd112136b6fe5e1a9
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2018-05-25 20:26:15 +0900

    Fix: asset/test/README.md, menu.phtml

--- asset/test/core/notice.php
+++ asset/test/core/notice.php
@@ -8,8 +8,17 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+?>
+<section id="testcase">
+	<h2>Notice of Core</h2>
+	<div>
+		| <a href="<?= ConvertURL('testcase:/unit/notice') ?>">Unit of Notice</a> |
+	</div>
+	<hr/>
+</section>
+<?php
 //	...
-Notice::Set("This is just test.");
+Notice::Set("This is just Notice test. (Not Unit of Notice)");
 
 //	...
 D(Notice::Get());
