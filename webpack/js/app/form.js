99eb8b026b24445f5326901e4752a60465d52a34
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-06-19 13:01:05 +0900

    Fix: webpack - js/app/form.js, css/app/form.css - Disable duplicate click feature.

--- webpack/js/app/form.js
+++ webpack/js/app/form.js
@@ -9,3 +9,25 @@
  * @copyright Tomoaki Nagahara All right reserved.
  */
 ?>
+//	Wait dom content loaded.
+document.addEventListener('DOMContentLoaded', function(){
+	//	Get the button tag under the form tag has OP class.
+	var buttons = document.querySelectorAll('FORM.OP BUTTON');
+
+	//	Set each buttons.
+	for(var i=0; i<buttons.length; i++){
+		//	Wait button click.
+		buttons[i].addEventListener("click", function( event ) {
+			//	Disable duplicate click.
+			event.target.disabled = true;
+			event.target.form.submit();
+
+			//	Disable other link click.
+			for(var a of document.querySelectorAll('a') ){
+				a.addEventListener("click", function( event ) {
+					event.preventDefault();
+				}, false);
+			}
+		}, false);
+	}
+}, false );
