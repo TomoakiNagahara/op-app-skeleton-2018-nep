cc7d3a83524dd7558560cacd7ad99564bf3a65f0
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2018-06-21 09:27:47 +0900

    NEW: asset/unit/orm/Selftest.class.php

---
+++ asset/unit/orm/Selftest.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * unit-orm:/Selftest.class.php
+ *
+ * @created   2018-06-21
+ * @version   1.0
+ * @package   unit-orm
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @created   2018-06-21
+ */
+namespace OP\UNIT\ORM;
+
+/** Selftest
+ *
+ * @created   2018-06-21
+ * @version   1.0
+ * @package   unit-orm
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Selftest
+{
+	/** trait
+	 *
+	 */
+	use \OP_CORE;
+}
