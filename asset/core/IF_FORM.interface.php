96612a2beb48543f641c2a98580e239b46c1a4a4
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-07-02 17:46:42 +0900

    FIX: asset/core/IF_DATABASE, IF_FORM, IF_ORM, IF_ORM_RECORD, IF_QQL, IF_SQL

--- asset/core/IF_FORM.interface.php
+++ asset/core/IF_FORM.interface.php
@@ -19,59 +19,74 @@
  */
 interface IF_FORM
 {
-	/** Get saved values.
+	/** Set configuration.
 	 *
-	 */
-	public function Get();
-
-	/** Set save values.
-	 *
-	 * @param	 array		 $args
-	 */
-	public function Set(array $args);
-
-	/** Set configuration
-	 *
+	 * @addition 2018-04-20
 	 * @param	 array		 $config
+	 * @return	 array		 $config
 	 */
 	public function Config(array $config);
 
-	/** Display form tag.
+	/** Start form. Output open FORM tag.
 	 *
+	 * @addition 2018-04-20
 	 */
 	public function Start();
 
-	/** Display close form tag.
+	/** Finish form. Output close FORM tag.
 	 *
+	 * @addition 2018-04-20
 	 */
 	public function Finish();
 
 	/** Display input label.
 	 *
+	 * @addition 2018-04-20
 	 * @param	 string		 $name
 	 */
 	public function Label($name);
 
 	/** Display input tag.
 	 *
+	 * @addition 2018-04-20
 	 * @param	 string		 $name
 	 */
 	public function Input($name);
 
-	/** Display posted value.
+	/** Display submitted value.
 	 *
+	 * @addition 2018-04-20
 	 * @param	 string		 $name
 	 */
 	public function Value($name);
 
 	/** Display error message.
 	 *
-	 * <pre>
-	 * <?php $form->Error('nickname','p.nickname') ?>
-	 * </pre>
-	 *
+	 * @addition 2018-04-20
 	 * @param	 string		 $name
 	 * @param	 string		 $tag
 	 */
-	public function Error($name, $tag=null);
+	public function Error($name);
+
+	/** Overwrite input attributes.
+	 *
+	 * @addition 2018-06-29
+	 * @param	 array		 $input
+	 */
+	public function SetInput(array $input);
+
+	/** Overwrite input options.
+	 *
+	 * @addition 2018-06-29
+	 * @param	 array		 $option
+	 */
+	public function SetOption(array $option);
+
+	/** Get submitted value.
+	 *
+	 * @addition 2018-06-29
+	 * @param	 string			 $name
+	 * @param	 string|array	 $value
+	 */
+	public function GetValue($name);
 }
