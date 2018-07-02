ffcbb08331e768f8953dc480ce3a38d27a1dd0f0
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-07-02 17:46:42 +0900

    FIX: asset/core/IF_DATABASE, IF_FORM, IF_ORM, IF_ORM_RECORD, IF_QQL, IF_SQL

--- asset/core/IF_ORM.interface.php
+++ asset/core/IF_ORM.interface.php
@@ -1,30 +1,34 @@
 <?php
 /**
- * IF_QQL.interface.php
+ * IF_ORM.interface.php
  *
- * @creation  2018-05-14
+ * @creation  2018-06-29
  * @version   1.0
  * @package   core
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
-/** IF_QQL
+/** IF_ORM
  *
- * @creation  2018-05-14
+ * @creation  2018-06-29
  * @version   1.0
  * @package   core
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-interface IF_QQL
+interface IF_ORM
 {
-	/** Parse to QQL from serial string.
+	/**
 	 *
-	 * @addition 2018-06-29
-	 * @param	 string		 $string
-	 * @param	 string		 $options
-	 * @return	 array		 $config
+	 * @param	 string	 $qql
+	 * @return
 	 */
-	static public function Parse($string, $options);
+	public function Find($qql);
+
+	/**
+	 *
+	 * @param unknown $record
+	 */
+	public function Save($record);
 }
