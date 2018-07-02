dce48e631b360e805a878d89542ba4cde1a4d1fc
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-07-02 17:42:12 +0900

    NEW: asset/unit/google/Google.class.php, OAuth.class.php

--- asset/unit/google/index.php
+++ asset/unit/google/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * index.php
+ * unit-google:/index.php
  *
  * @creation  2017-06-08
  * @version   1.0
@@ -8,18 +8,5 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-//	Setup auto loader.
-spl_autoload_register( function ($name){
-	try {
-		//	...
-		$path = __DIR__."/{$name}.class.php";
-
-		//	...
-		if( file_exists($path) ){
-			include($path);
-		}
-	} catch ( Throwable $e ){
-		Notice::Set($e);
-	}
-});
-return true;
+//	...
+include('Google.class.php');
