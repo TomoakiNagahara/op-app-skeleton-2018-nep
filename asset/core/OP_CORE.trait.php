b77858555cc591fd8dd6e6c867a76665b762d265
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-07-02 17:48:25 +0900

    Fix: asset/core/OP_CORE - Fixed debug_backtrace argument at Notice::Set().

--- asset/core/OP_CORE.trait.php
+++ asset/core/OP_CORE.trait.php
@@ -38,10 +38,14 @@ trait OP_CORE
 					$join[] = $val;
 			}
 		}
+
+		//	...
 		$class   = get_class($this);
 		$serial  = join(', ', $join);
 		$message = "This method has not been exists. ({$class}->{$name}({$serial}))";
-		Notice::Set($message, debug_backtrace());
+
+		//	...
+		Notice::Set($message, debug_backtrace(false));
 	}
 
 	/** Calling to has not been set static method.
@@ -52,7 +56,7 @@ trait OP_CORE
 	static function __callstatic($name, $args)
 	{
 		$message = "This method has not been exists. ($name)";
-		Notice::Set($message, debug_backtrace());
+		Notice::Set($message, debug_backtrace(false));
 	}
 
 	/** Calling to has not been set property.
@@ -62,7 +66,7 @@ trait OP_CORE
 	function __get($name)
 	{
 		$message = "This property has not been exists. ($name)";
-		Notice::Set($message, debug_backtrace());
+		Notice::Set($message, debug_backtrace(false));
 	}
 
 	/** Calling to has not been set property.
@@ -73,7 +77,7 @@ trait OP_CORE
 	function __set($name, $args)
 	{
 		$message = "This property has not been exists. ($name)";
-		Notice::Set($message, debug_backtrace());
+		Notice::Set($message, debug_backtrace(false));
 	}
 
 	/** Enumerate property names to serialize.
