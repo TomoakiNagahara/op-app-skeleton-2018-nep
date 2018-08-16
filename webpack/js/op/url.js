c9311acc541d49013dd0cad901d3984f87e041bc
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-08-16 18:00:47 +0900

    New: webpack/js/op/url.js

---
+++ webpack/js/op/url.js
@@ -0,0 +1,92 @@
+/**
+ * app-skeleton-webpack:/js/op/url.js
+ *
+ * @creation  2017-06-08
+ * @version   1.0
+ * @package   app-skeleton
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+if( typeof $OP.URL === "undefined" ){
+	$OP.URL = {};
+}
+
+//	...
+(function(){
+	//	...
+	var queries = {};
+
+	//	...
+	location.search.substr(1).split('&').map(function(v){
+		var m = v.match(/^([^=]+)(=)(.+)/);
+		if( m ){
+			//	...
+			var key = m[1];
+			var val = decodeURIComponent(m[3].replace(/\+/,' '));
+
+			//	For checkbox.
+			if( key.match(/%5B%5D$/) ){
+				key = key.replace(/%5B%5D$/,'');
+				if( queries[key] === undefined ){
+					queries[key] = [];
+				}
+D(val)
+				//	...
+				queries[key].push(val);
+			}else{
+				queries[key] = val;
+			};
+		};
+	});
+
+	//	...
+	$OP.URL.Protocol = function(){
+		return location.Protocol;
+	};
+
+	//	...
+	$OP.URL.Domain = function(){
+		return location.host;
+	};
+
+	//	...
+	$OP.URL.Query = {};
+
+	//	...
+	$OP.URL.Query.Get = function(key, def){
+		if( queries[key] ){
+			return queries[key];
+			/*
+			return decodeURI(queries[key])
+					.replace(/%2F/g,'/')
+					.replace(/%3F/g,'?');
+			*/
+		}else if( def === null ){
+			//	Web storage
+		}else{
+			return def ? def: null;
+		}
+	};
+
+	//	...
+	$OP.URL.Query.Set = function(key, val, save){
+		queries[key] = val
+		window.history.pushState(null, null, __generate());
+
+		//	...
+		if( save ){
+			//	Web storage
+		}
+	}
+
+	//	...
+	function __generate(){
+		var url = '?';
+		for(var key in queries ){
+			var val =  queries[key];
+			url += key+"="+val+'&';
+		}
+		return url.slice(0, -1);
+	};
+})();
