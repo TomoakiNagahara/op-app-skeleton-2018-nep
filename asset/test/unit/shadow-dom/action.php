2043d246282a35fd7f72121b868d53cf5b9b5116
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-09-05 17:09:50 +0900

    New: asset/test/unit/shadow-dom

---
+++ asset/test/unit/shadow-dom/action.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-test:/unit/shadow-dom/action.php
+ *
+ * @creation  2018-09-04
+ * @version   1.0
+ * @package   unit-test
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('test-2.html');
