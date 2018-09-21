32b1d8906b87d95649d139c1236c4b94865e834a
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-09-21 14:48:42 +0900

    Fix: asset/test/unit/shadow-dom/test-5.html, test-5-sdom.js

--- asset/test/unit/shadow-dom/test-5-sdom.js
+++ asset/test/unit/shadow-dom/test-5-sdom.js
@@ -40,6 +40,10 @@
 
 				//	...
 				if( name === dom.getAttribute('name') ){
+					//	...
+					__parser(dom);
+
+					//	...
 					__list[tag][name] = dom;
 					break;
 				};
@@ -47,14 +51,39 @@
 		};
 
 		//	...
+		if(!__list[tag][name] ){
+			console.error(`Has not been found this dom. (${tag}, ${name})`);
+			return false;
+		};
+
+		//	...
 		return __list[tag][name];
+	};
+
+	//	...
+	function __parser(dom){
+		//	...
+		for(var tag of ['script','style']){
+			var st = dom.innerHTML.indexOf(`<${tag}>`);
+			var en = dom.innerHTML.indexOf(`</${tag}>`);
+			if( st !== -1 && en !== -1 ){
+				//	...
+				var ad = 2 + tag.length;
+
+				//	...
+				D( dom.innerHTML.slice(st+ad,en) );
+
+				//	...
+				dom.innerHTML = dom.innerHTML.substr(0, st);
+			};
+		};
 	};
 })();
 
 //...
 var ShadowDom = function(name){
 	//	...
-	this.__name = name;
+	this.__name   = name;
 
 	//	...
 	var dom = $OP.SDOM.Get(document, 'sdom', name);
@@ -72,16 +101,14 @@ ShadowDom.prototype.Build = function(){
 	var dom = document.createElement('div');
 		dom.innerHTML = this.__html;
 
-	/*
 	//	...
-	this.__Json(dom);
+//	this.__Json(dom);
 
 	//	...
 	this.__Dom(dom);
 
 	//	...
 	this.__On(dom);
-	*/
 
 	//	...
 	var name_1 = this.__name;
@@ -89,7 +116,12 @@ ShadowDom.prototype.Build = function(){
 
 	//	...
 	var temp = $OP.SDOM.Get(document, this.__name, this.__insert_name);
-		temp.innerHTML = dom.innerHTML;
+	if(!temp ){
+		return;
+	};
+
+	//	...
+	temp.innerHTML = dom.innerHTML;
 };
 
 //...
@@ -111,4 +143,104 @@ ShadowDom.prototype.Update = function(){
 ShadowDom.prototype.Remove = function(){
 	//	...
 	this.__insert_point.innerHTML = '';
+};
+
+//...
+ShadowDom.prototype.__Dom = function(dom){
+
+	//	...
+	this.__If( dom.querySelectorAll(':scope > [if]') );
+
+	//	...
+	this.__For( dom.querySelectorAll(':scope > [for]') );
+
+	//	...
+	for(var i=0; i<dom.childNodes.length; i++){
+		//	...
+		var node = dom.childNodes[i];
+
+		//	...
+		if(!node.tagName ){
+			continue;
+		};
+
+		//	...
+		this.__Dom( node );
+	};
+};
+
+//...
+ShadowDom.prototype.__If = function(list){
+	//	...
+	for(var i=0; i<list.length; i++){
+		var dom = list[i];
+		var val = dom.getAttribute('if');
+		if(!eval( val ) ){
+			dom.parentNode.removeChild(dom);
+		};
+	};
+};
+
+//...
+ShadowDom.prototype.__For = function(list){
+	//	...
+	for(let i=0; i<list.length; i++){
+		var dom = list[i];
+		var tmp = dom.getAttribute('for');
+		if( tmp === "{ this.json }" ){
+			tmp = dom.getAttribute('json');
+			if( tmp ){
+				tmp = tmp.replace(/\\'/g  , '&#39;');
+				tmp = tmp.replace(/'/g    , '"');
+				tmp = tmp.replace(/&#39;/g, "'");
+			};
+		};
+		var val = JSON.parse(tmp);
+		var temp= '';
+
+		//	...
+		for(let i in val){
+			let v = val[i];
+
+			//	...
+			var html = dom.innerHTML;
+
+			//	...
+			html = html.replace(/\{\s*i\s*\}/g, i);
+			html = html.replace(/\{\s*v\s*\}/g, v);
+
+			//	...
+			html = html.replace(/\{\s*index\s*\}/g, i);
+			html = html.replace(/\{\s*value\s*\}/g, v);
+
+			//	...
+			temp += html;
+		};
+
+		//	...
+		dom.innerHTML  = '';
+		dom.innerHTML += temp;
+	};
+};
+
+//...
+ShadowDom.prototype.__On = function(dom){
+	//	...
+	for(var attr of ['onclick','onmouseover','onmouseleave']){
+
+		//	...
+		var list = dom.querySelectorAll(`[${attr}]`);
+
+		//	...
+		for(var i=0; i<list.length; i++){
+			var temp = list[i];
+
+			//	...
+			var sdom_name = this.__name;
+			var func_name = temp.getAttribute(attr);;
+
+			//	...
+			temp.setAttribute(attr, `return $OP.SDOM.Action.Exe('${sdom_name}', '${func_name}');`);
+		};
+	};
 };
