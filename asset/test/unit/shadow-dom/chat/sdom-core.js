3f51ae0dbb269f6ba7f355a377208bcfa83712be
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-10-16 16:15:45 +0900

    New: asset/test/unit/shadow-dom/chat

---
+++ asset/test/unit/shadow-dom/chat/sdom-core.js
@@ -0,0 +1,11 @@
+/**
+ * unit-test:/unit/shadow-dom/chat/sdom-core.js
+ *
+ * @creation  2018-10-16
+ * @version   1.0
+ * @package   unit-test
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+(function(){})();
