62637ba485a6d5558a9699a6a630f6c01a2c6de6
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-10-16 16:15:45 +0900

    New: asset/test/unit/shadow-dom/chat

---
+++ asset/test/unit/shadow-dom/chat/action.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * unit-test:/unit/shadow-dom/chat/action.php
+ *
+ * @creation  2018-10-16
+ * @version   1.0
+ * @package   unit-test
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('index.html');
+include('app.html');
+include('sdom-core.js');
+include('sdom-public.js');
+include('sdom-private.js');
