6d0b6e7b8e841e75dd2dc6b72a904f2e6da790c3
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-10-26 17:05:31 +0900

    New: asset/layout/chameleon

---
+++ asset/layout/chameleon/css/action.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * layout-chameleon:/css/action.php
+ *
+ * @creation  2018-10-26
+ * @version   1.0
+ * @package   layout-chameleon
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+return [
+	__DIR__.'/root',
+	__DIR__.'/body',
+	__DIR__.'/color',
+];
---
+++ asset/layout/chameleon/js/action.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * layout-chameleon:/js/action.php
+ *
+ * @creation  2018-10-26
+ * @version   1.0
+ * @package   layout-chameleon
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+return [
+	__DIR__.'/init-hue',
+];
