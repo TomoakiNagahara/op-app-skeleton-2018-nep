775f280e55a7d9da1e9c52dd8b53e0cfcc584b45
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-10-30 12:55:11 +0900

    New: asset/reference

---
+++ asset/reference/core/action.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * unit-reference:/core/action.php
+ *
+ * @creation  2018-10-30
+ * @version   1.0
+ * @package   unit-reference
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
---
+++ asset/reference/index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * unit-referencet:/index.php
+ *
+ * @creation  2018-10-30
+ * @version   1.0
+ * @package   unit-reference
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+$endpoint  = App::EndPoint();
+$reference = basename(dirname($endpoint));
+
+//	...
+App::Breadcrumbs(['href'=>'app:/','text'=>'TOP']);
+
+//	...
+App::Breadcrumbs(['href'=>'app:/'.$reference,'text'=>'Reference']);
+
+//	...
+$args = [$reference];
+foreach( App::Args() as $arg ){
+	$args[] = $arg;
+	$list['text'] = ucfirst($arg);
+	$list['href'] = 'app:/'.join('/', $args);
+	App::Breadcrumbs($list);
+}
+
+//	...
+$action = $args[0].'/action.php';
+
+//	...
+if( file_exists($action) ){
+	include($action);
+}
