dd13baae0b254069a462b4670b672ff15392004a
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-11-01 14:57:01 +0900

    Fix: webpack/js/op/path.js - $OP.Path.Convert() is deprecated. Wrapping to $OP.URL.Convert().

--- webpack/js/op/path.js
+++ webpack/js/op/path.js
@@ -71,68 +71,10 @@ $OP.Path.Compress = function( path ){
  *
  * @creation  2017-06-07
  * @version   1.0
- * @package   app-webpack-js
+ * @package   app-skeleton-webpack
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
 $OP.Path.Convert = function( path ){
-	//	Check of variable type.
-	if( typeof path !== 'string' ){
-		console.log('This argument type is not string. (' + typeof path + ')');
-		console.dir(path);
-		return;
-	}
-
-	//	Search meta label.
-	var m = path.match(/^([\w]+):\//);
-
-	//	Get meta label
-	var meta = m[1] ? m[1] : null;
-
-	//	Replace to real path from meta label.
-	switch( meta === null ){
-		case null:
-		case 'http':
-		case 'https':
-			return path;
-		default:
-	};
-
-	//	Convert to real path.
-	if(!$OP.Path.meta[meta] ){
-		console.error(`This meta name has not been set. (${meta})`);
-		return false;
-	};
-
-	//	Convert to full path.
-	path = $OP.Path.meta[meta] + path.substr( meta.length +2 );
-
-	//	Remove document-root path.
-	path = path.substr( $OP.Path.meta.doc.length -1 );
-
-	//	Separate to url query.
-	var pos = path.indexOf('?');
-	if( pos === -1 ){
-		var url = path;
-		var que = '';
-	}else{
-		var url = path.substr(0, pos);
-		var que = path.substr(pos);
-	};
-
-	//	Search file name from tail of URL.
-	var pos = url.lastIndexOf('/');
-	var str = url.substr(pos+1);
-	var pos = str.indexOf('.');
-
-	//	Add slash. Anti apache's automatically transfer.
-	if( str && pos === -1 ){
-		url += '/';
-	};
-
-	//	Join
-	path = url + que;
-
-	//	Return.
-	return path;
+	return $OP.URL.Convert(path);
 }
