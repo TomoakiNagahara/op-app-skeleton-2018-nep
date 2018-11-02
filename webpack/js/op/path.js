44f9a84c76f8ca15208cd71644bb3c82a9086c5a
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2018-11-03 02:24:44 +0900

    New: webpack/js/op/path.js - $OP.Path.CompressPath(), Re:write again.

--- webpack/js/op/path.js
+++ webpack/js/op/path.js
@@ -32,4 +32,44 @@ $OP.Path = {};
  */
 $OP.Path.Convert = function( path ){
 	return $OP.URL.Convert(path);
-}
+};
+
+/**
+ * @creation  2018-11-03
+ * @version   1.0
+ * @package   app-skeleton-webpack
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+(function(){
+	var __meta = {};
+//	<?php if( Env::isAdmin() ): ?>
+		__meta.op  = "<?= ConvertPath('op:/' ); ?>";
+		__meta.app = "<?= ConvertPath('app:/'); ?>";
+		__meta.doc = "<?= ConvertPath('doc:/'); ?>";
+//	<?php endif; ?>
+
+	/** Compress path.
+	 *
+	 * @param   string  path
+	 * @return  string  path
+	 */
+	$OP.Path.Compress = function( path ){
+		//	...
+		if(!path ){
+			return false;
+		};
+
+		//	...
+		for(var meta in __meta){
+			//	...
+			if( path.indexOf( __meta[meta] ) === 0 ){
+				path = path.replace(__meta[meta], meta+':/');
+				break;
+			};
+		};
+
+		//	...
+		return path;
+	};
+})();
