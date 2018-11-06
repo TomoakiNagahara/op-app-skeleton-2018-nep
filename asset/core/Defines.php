e3593c9cde10949a510f3f68d0d523ca03f96846
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-11-06 15:07:09 +0900

    Fix: asset/core/Defines.php - For PHP 7.3 will declaration of case-insensitive constants is deprecated.

--- asset/core/Defines.php
+++ asset/core/Defines.php
@@ -13,34 +13,34 @@
  *
  * @var string
  */
-define('OP_ROOT', 'OP_ROOT', true);
+define('OP_ROOT', 'OP_ROOT', false);
 
 /** onepiece-framework's application installed path.
  *
  * @var string
  */
-define('APP_ROOT', 'APP_ROOT', true);
+define('APP_ROOT', 'APP_ROOT', false);
 
 /** Web server's document root path.
  *
  * @var string
  */
-define('DOC_ROOT', 'DOC_ROOT', true);
+define('DOC_ROOT', 'DOC_ROOT', false);
 
 /** Namespace
  *
  * @var string
  */
-define('_OP_NAME_SPACE_', 'ONEPIECE', true);
+define('_OP_NAME_SPACE_', 'ONEPIECE', false);
 
 /** App ID
  *
  * @var string
  */
-define('_OP_APP_ID_', 'APP_ID', true);
+define('_OP_APP_ID_', 'APP_ID', false);
 
 /** Use to salt.
  *
  * @var string
  */
-define('_OP_SALT_', 'onepiece-framework', true);
+define('_OP_SALT_', 'onepiece-framework', false);
