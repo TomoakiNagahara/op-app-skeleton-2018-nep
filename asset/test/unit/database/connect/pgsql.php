8d051ba489ced84572384ba19a9915200a689b2d
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-12-14 18:17:25 +0900

    New: asset/test/unit/database/connect/pgsql.php

---
+++ asset/test/unit/database/connect/pgsql.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * unit-test:/unit/database/pgsql.php
+ *
+ * @creation  2018-12-14
+ * @version   1.0
+ * @package   unit-test
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+/* @var $db OP\UNIT\Database */
+$db = Unit::Instantiate('Database');
+
+//	...
+$config = [
+	'prod'     => 'pgsql',
+	'host'     => 'localhost',
+	'port'     => '5432',
+	'database' => 'database_name',
+	'role'     => 'testuser',
+	'password' => 'password',
+	'charset'  => 'utf-8',
+];
+
+//	...
+$db->Connect($config);
+
+//	...
+return $db;
