a21d8cb05cd4f648558ba3f73f229a53c1042da1
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-12-17 17:10:18 +0900

    New: asset/test/unit/database/create/user/action.php

---
+++ asset/test/unit/database/create/user/action.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * unit-test:/unit/database/create/user.php
+ *
+ * @creation  2018-12-17
+ * @version   1.0
+ * @package   unit-test
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+foreach( [] as $prod ){
+	include("{$prod}.inc.php");
+};
