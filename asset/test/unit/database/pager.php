258009ae4389ce7f33a5d10969fa7988aa39106d
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2018-12-26 13:26:29 +0900

    Fix: asset/test/unit/database/pager.php - Optimized to newer of Pager class.

--- asset/test/unit/database/pager.php
+++ asset/test/unit/database/pager.php
@@ -18,7 +18,12 @@ if(!$db->isConnect() ){
 
 /* @var $pager \OP\UNIT\Pager */
 $pager  = Unit::Instance('Pager');
-$config = $pager->Config(['database'=>'testcase', 'table'=>'t_orm'], $db);
+$config = $pager->Config([
+	'database' => 'testcase',
+	'table'    => 't_orm',
+	'limit'    =>  $_GET['limit'] ?? 10,
+	'wings'    =>  $_GET['wings'] ??  2,
+], $db);
 
 //  Display pager.
 $pager->Display();
@@ -27,59 +32,7 @@ $pager->Display();
 $record = $db->Select($config);
 
 //	...
+$pager->Debug();
+
+//	...
 D( $record );
-
-//	...
-return;
-
-/*
-
-//	...
-$config = [];
-$config['table'] = 't_orm';
-$config['where']['ai']['evalu'] = '!=';
-$config['where']['ai']['value'] = null;
-$count = $db->Count($config);
-
-//	...
-$ppr  = 10;
-$page = ifset($_GET['page'], 1);
-$max  = ceil($count / $ppr);
-
-//	...
-if( $page > $max ){
-	$page = $max;
-}
-
-//	...
-if( $page < 1 ){
-	$page = 1;
-}
-
-//	...
-$offset = ($page -1) * $ppr;
-
-//	...
-$config['limit']  = $ppr;
-$config['offset'] = $offset;
-
-//	...
-print '<div class="pager">'.PHP_EOL;
-printf('<span class="page"><a href="?%s"></a></span>'.PHP_EOL, http_build_query(array_merge($_GET, ['page'=>1])) );
-for( $i=1; $i<=$max; $i++ ){
-	if( $i === $page ){
-		printf('<span class="page current">%s</span>'.PHP_EOL, $i);
-	}else{
-		printf('<span class="page"><a href="?page=%s">%s</a></span>'.PHP_EOL, $i, $i);
-	}
-}
-printf('<span class="page"><a href="?%s"></a></span>'.PHP_EOL, http_build_query(array_merge($_GET, ['page'=>$max])) );
-print '</div>'.PHP_EOL;
-
-//	...
-D('total record number', $count);
-D('current page',		 $page);
-D('page per record',	 $ppr);
-D('maximum page',		 $max);
-
-*/
