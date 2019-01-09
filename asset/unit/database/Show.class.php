444dad053e8761d28bd98d82df74a7efe0419982
Tomoaki -imac5k- <tomoaki.nagahara+imac5k@gmail.com>
2019-01-09 11:53:26 +0900

    New: asset/unit/database/Show::_Column(), _Index(), _User()

--- asset/unit/database/Show.class.php
+++ asset/unit/database/Show.class.php
@@ -58,4 +58,124 @@ class Show
 			return $records;
 		}
 	}
+
+	/** Column
+	 *
+	 * @param  array $records
+	 * @return array $result
+	 */
+	static private function _Column($records)
+	{
+		//	...
+		$result = [];
+
+		//	...
+		foreach( $records as $record ){
+			//	...
+			$name = $record['Field'];
+
+			//	...
+			foreach( $record as $key => $val ){
+				//	...
+				if( $key === 'Collation' or $key === 'Default' ){
+					if( $val === null ){
+						continue;
+					}
+				}
+
+				//	...
+				$key = lcfirst($key);
+
+				//	...
+				if( $key === 'type' ){
+					//	Parse --> type unsigned --> type, unsigned
+					if( $pos = strpos($val, 'unsigned') ){
+						$val = substr($val, 0, $pos-1);
+						$result[$name]['unsigned'] = true;
+					}
+
+					//	Parse --> type(length) --> type, length
+					if( $st = strpos($val, '(') and $en = strpos($val, ')') ){
+						$type   = substr($val, 0, $st);
+						$length = substr($val, $st+1, $en - $st -1 );
+
+						//	...
+						if( is_numeric($length) ){
+							$length = (int)$length;
+						}
+
+						//	...
+						$result[$name]['type']   = $type;
+						$result[$name]['length'] = $length;
+
+						//	...
+						continue;
+					}
+				}
+
+				//	...
+				if( $key === 'null' ){
+					$val = $val === 'YES' ? true: false;
+				}
+
+				//	...
+				if( $key === 'key' ){
+					$val = strtolower($val);
+				}
+
+				//	...
+				$result[$name][$key] = $val;
+			}
+		}
+
+		//	...
+		return $result;
+	}
+
+	/** Index
+	 *
+	 * @param  array $records
+	 * @return array $result
+	 */
+	static private function _Index(array $records)
+	{
+		//	...
+		$result = [];
+
+		//	...
+		foreach( $records as $record ){
+			//	...
+			$name = $record['Key_name'];
+			$seq  = $record['Seq_in_index'];
+			$result[$name][$seq] = $record;
+		}
+
+		//	...
+		return $result;
+	}
+
+	/** User
+	 *
+	 * @param  array $records
+	 * @return array $result
+	 */
+	static private function _User(array $records)
+	{
+		//	...
+		$result = [];
+
+		//	...
+		foreach( $records as $record ){
+			//	...
+			$host     = $record['host']     ?? 'localhost';
+			$user     = $record['user']     ?? $record['usename'] ?? null;;
+		//	$password = $record['password'] ?? null;
+
+			//	...
+			$result[$host][$user] = true;
+		}
+
+		//	...
+		return $result;
+	}
 }
